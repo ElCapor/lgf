d109b76d7c43b23c2b94e1f4ec66ee92fe405bdc
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-01-09 20:36:50 +0100

    Rewritten stuff

--- src/libs/events/include/events.hpp
+++ src/libs/events/include/events.hpp
@@ -2,22 +2,80 @@
 #ifndef EVENTS_HPP
 #define EVENTS_HPP
 
-// declare event as a template to have run time checks
-template <typename T>
-class Event {
-
-}
-
-template <typename T>
-class EventConnector
-{
-    
-}
-
-class EventSystem
+class Event
 {
 public:
-std::map<   
+  virtual~ Event();
+
+  using DescriptorType const char*;
+
+  virtual DescriptorType type() const = 0;
+};
+
+
+class DemoEvent : public Event
+{
+public:
+  DemoEvent();
+  virtual ~DemoEvent();
+
+  static constexpr DescriptorType descriptor = "DemoEvent";
+
+  virtual DescriptorType type() const
+  {
+    return descriptor;
+  }
+};
+
+
+class Dispatcher
+{
+public:
+
+  using SlotType = std::function< void( const Event& ) >;
+
+  void subscribe( const Event::DescriptorType& descriptor, SlotType&& slot );
+
+  void post( const Event& event ) const;
+
+private:
+
+ std::map< Event::DescriptorType, std::vector<SlotType> > _observers;
+};
+
+
+void Dispatcher::subscribe( const Event::DescriptorType& descriptor, SlotType&& slot )
+{
+  _observers[descriptor].push_back( slot );
 }
+
+void Dispatcher::post( const Event& event ) const
+{
+  auto type = event.type();
+
+  // Ignore events for which we do not have an observer (yet).
+  if( _observers.find( type ) == _observers.end() )
+    return;
+
+  auto&& observers = _observers.at( type );
+
+  for( auto&& observer : observers )
+    observer( event );
+}
+
+class ClassObserver
+{
+public:
+  void handle( const Event& e )
+  {
+    if( e.type() == DemoEvent::descriptor )
+    {
+      // This demonstrates how to obtain the underlying event type in case a
+      // slot is set up to handle multiple events of different types.
+      const DemoEvent& demoEvent = static_cast<const DemoEvent&>( e );
+      std::cout << __PRETTY_FUNCTION__ << ": " << demoEvent.type() << std::endl;
+    }
+  }
+};
 
 #endif
